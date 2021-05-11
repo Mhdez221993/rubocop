e884c242e800ca8b47dc3477e1818119fc04a4d2
Moises <mhdezcoronado@gmail.com>
2021-05-11 11:00:54 +0200

    fix regex on indentatin class

--- lib/indentation.rb
+++ lib/indentation.rb
@@ -10,20 +10,22 @@ class Indentation
   def check_indentation
     outer = 0
     File.readlines(file, chomp: true).each_with_index do |line, index|
-      offenses = "#{file}: #{index + 1}: #{line}: #{outer}: #{line.index(/[^ ]/)}"
+      space = line.index(/[^ ]/)
+      offenses = "#{file}: #{index + 1}: #{outer} #{space}"
       if find_method_or_class(line)
-        class_offense.push_offenses(offenses) unless outer == line.index(/[^ ]/)
+        # class_offense.push_offenses(offenses) unless outer == line.index(/[^ ]/)
         outer += 2
       elsif find_end_keyword(line)
         outer -= 2
+        # class_offense.push_offenses(offenses) if outer != line.index(/[^ ]/)
+      elsif !line.empty?
         class_offense.push_offenses(offenses) if outer != line.index(/[^ ]/)
       end
     end
   end
 
   def find_method_or_class(str)
-    return true if (str =~ /(^\s*if)/) == 0
-    str =~ /(class\s)|(def\s)|(do\s)/
+    str =~ /(class\s)|(def\s)|(do\s)|(^\s*if)/
   end
 
   def find_end_keyword(str)
