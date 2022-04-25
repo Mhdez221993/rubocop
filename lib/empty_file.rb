c68847d8af1962ff22311b669cec0d55f86ac534
Moises <mhdezcoronado@gmail.com>
2022-04-25 23:57:59 +0200

    Update empty_file method

--- lib/empty_file.rb
+++ lib/empty_file.rb
@@ -9,14 +9,14 @@ class EmptyFile
   end
 
   def check_if_empty_file
-    class_offense.push_offenses("#{file}:1:1: w: Lint/EmtyFile: Empty file detected") if File.zero?(file)
+    class_offense.push_offenses(" #{file}:1:1: w: Lint/EmtyFile: Empty file detected") if File.zero?(file)
   end
 
   def check_last_line
     message = 'C: Layout/TrailingEmptyLines: Final newline missing'
     f = File.readlines(file)
     line = f[f.size - 1]
-    class_offense.push_offenses("#{file}:#{file.size + 1}:#{line.size + 1}: " + message) unless line =~ /\n$/
+    class_offense.push_offenses(" #{file}:#{file.size + 1}:#{line.size + 1}: " + message) unless line =~ /\n$/
   end
 
   private
